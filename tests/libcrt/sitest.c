b43acaa1a0155dc09d71fb5d45d539c749761191
Earnie Boyd <earnie@users.sourceforge.net>
2012-09-07 08:53:13 -0400

    Apply LICENSE to all files as appropriate.
    	* tests/Makefile.in: Add file preamble.
    	* tests/headers/directx.c: Ditto.
    	* tests/headers/test.c: Ditto.
    	* tests/libcrt/sitest.c: Ditto.
    	* tests/libcrt/testwmem.c: Ditto.
    	* tests/libcrt/tst-aligned-malloc.c: Ditto.

--- tests/libcrt/sitest.c
+++ tests/libcrt/sitest.c
@@ -1,9 +1,29 @@
+/**
+ * @file sitest.c
+ * @copy 2012 MinGW.org project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 /*
 	sitest -- exercise features of C99 <stdint.h> and <inttypes.h>
-
-	This source code has been placed into the PUBLIC DOMAIN by its author.
-
-	last edit:	1999/11/05	gwyn@arl.mil
 
 	Tries to accommodate pre-C99 versions of <inttypes.h>.
 
